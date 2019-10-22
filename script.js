261f00ca99ac17c63ac99a6ebab1b8eda84ca0ba
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-22 14:35:58 -0400

    Added an accordion so all trivia questions are included inside accordion

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 // alert("Hello World");
 let form = document.getElementById("quiz");
-form.addEventListener("submit", checkAnswer); 
+// form.addEventListener("submit", checkAnswer);
+let submit = document.getElementById("submit");
+submit.addEventListener("click", checkAnswer);
 let correct = 0;
+let accordion = document.getElementsByClassName("accordion");
 
-function checkAnswer(evt){
+for(let i=0; i < accordion.length; i++){
+    accordion[i].addEventListener("click", function(evt){
+        evt.preventDefault();
+        this.classList.toggle("active"); 
+        let panel = this.nextElementSibling;
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+        } else{
+            panel.style.display = "block";
+        }
+    })
+}
+function checkAnswer(evt) {
     evt.preventDefault();
-    if(form.question1.value == "Cut off their toes and heels"){
+    if (form.question1.value == "Cut off their toes and heels") {
         console.log("Correct!");
         correct++;
         console.log(correct);
-    }else{
+    } else {
         console.log("Wrong")
         console.log(correct);
     }
 
 
-    if(form.question2.value == "She was forced to wear a pair of red hot iron shoes until she dropped dead"){
+    if (form.question2.value == "She was forced to wear a pair of red hot iron shoes until she dropped dead") {
         console.log("Correct");
         correct++
         console.log(correct);
     }
-    else{
+    else {
         console.log("Wrong");
     }
-    if(form.question3.value == "Rapunzel's clothes no longer fit because she was pregnant"){
+    if (form.question3.value == "Rapunzel's clothes no longer fit because she was pregnant") {
         console.log("Correct");
         correct++;
         console.log(correct);
@@ -32,14 +47,14 @@ function checkAnswer(evt){
 
 
 
-//SCORING 
-    if(correct === 3){
+    //SCORING 
+    if (correct === 3) {
         console.log("You got them all right!")
     }
-    else if(correct < 3 && correct > 0){
+    else if (correct < 3 && correct > 0) {
         console.log("You seem to know a bit");
     }
-    else if( correct === 0){
+    else if (correct === 0) {
         console.log("Yikes. You got 'em all wrong. Better brush up on your fairy tales");
     }
 
