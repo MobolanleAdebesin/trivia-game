34fac5097eaaf74818c410082f2f40f02a9121c5
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-22 20:29:49 -0400

    created template for using javascript to fill html elements

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
 // alert("hello world")
+let heading = document.getElementById("heading")
+let labels = document.getElementsByTagName("label"); 
+let inputs = document.getElementsByTagName("input");
+let paragraphs = document.getElementsByTagName("p");
+let image = document.getElementById("main-image");
+//Each question will include: 
+//One heading with the question. 
+//One image 
+//four labels 
+//four inputs 
+//four paragraphs 
+
+let question1 = {
+    heading: "In Cinderalla, how did her step-sisters attempt to prove they were the mystery woman from the ball?", 
+    image: "glass-slipper.gif", 
+    input: [
+        "Cut off their toes and heels", 
+        "Attempt to murder Cinderella", 
+        "Steal the glass slipper", 
+        "Kidnap the prince"
+    ]
+    }
+
+console.log(labels);
+console.log(inputs);
+console.log(paragraphs)
+console.log(image);
+console.log(heading);
+
+// console.log(question1);
+// console.log(question1.heading);
+// console.log(question1.image);
+
+// for(let i = 0; i < question1.input.length; i++ ){
+//     console.log(question1.input[i]);
+// }
+
+for(let i = 0; i < labels.length; i++){
+    heading.innerText = question1.heading; 
+    image.setAttribute("src", question1.image); 
+    inputs[i].setAttribute("value", question1.input[i]);
+    paragraphs[i].innerText = question1.input[i];
+
+}
+
