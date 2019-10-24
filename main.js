56b9e2607c84611b3ccfbd90a60c6791de2b3fc1
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-24 15:32:11 -0400

    Added a shuffle method to the bank class so that the questions are in a different order each time

--- main.js
+++ main.js
@@ -12,11 +12,13 @@ let questionNumber = document.querySelector("#questionNumber")
 let submitButton = document.querySelector("#submit-btn");
 let nextButton = document.querySelector('#next-btn');
 let form = document.querySelector("#quiz");
+let labelContainer = document.querySelector("#label-container");
 let correct = ["Cut off their toes and heels",
     "She was forced to wear a pair of red hot iron shoes until she dropped dead",
     "Rapunzel's clothes no longer fit because she was pregnant",
     "The nudging of her newborn twins awokened the princess",
-    "The prince marries someone else, the little mermaid throws herself into the ocean and turns to foam"
+    "The prince marries someone else, the little mermaid throws herself into the ocean and turns to foam",
+    "The princess smashes the frog against the wall"
 ]
 class Question {
     constructor(heading, inputs, image) {
@@ -24,11 +26,11 @@ class Question {
         this.inputs = inputs;
         this.image = image;
     }
-
 }
 class Bank {
     constructor() {
         this.count = 0;
+        this.score = 0;
         this.questions = [];
     }
     createQuestions() {
@@ -37,44 +39,74 @@ class Bank {
             "In Snow White and the Seven Dwarves, how was the wicked queen punished at the end of the story?",
             "In the story of Rapunzel, how did the witch know Rapunzel had been visited by the prince?",
             "How was sleeping beauty awakened from her slumber in the Brothers Grimm version of the tale?",
-            "How does the original story of the little mermaid end?"];
+            "How does the original story of the little mermaid end?",
+            "In the story, The Frog Prince, how does the princess turn the prince back into a human?"];
         let allInputs = [
             ["Cut off their toes and heels", "Attempt to murder Cinderella", "Steal the glass slipper", "Kidnap the prince"],
             ["She was banished from the kingdom", "The hunter cut out her heart and liver", "She was forced to wear a pair of red hot iron shoes until she dropped dead", "The seven dwarves made her their maid for the rest of her life"],
             ["She spied on the pair and saw the prince climbing Rapunzel's hair", "Rapunzel's clothes no longer fit because she was pregnant", "Rapunzel said 'You pull much harder than the prince when you climb my hair", "The prince left behind a lock of hair"],
             ["True love's kiss awokened the princess", "The nudging of her newborn twins awokened the princess", "The pain from a dagger awokened the princess", "A special antidote awokened the princess"],
-            ["The little mermaid and the prince marry", "The little mermaid turns back into a mermaid and never sees the prince again", "The prince banishes the little mermaid when he sees her talking to fish", "The prince marries someone else, the little mermaid throws herself into the ocean and turns to foam"]
-
+            ["The little mermaid and the prince marry", "The little mermaid turns back into a mermaid and never sees the prince again", "The prince banishes the little mermaid when he sees her talking to fish", "The prince marries someone else, the little mermaid throws herself into the ocean and turns to foam"],
+            ["The princess smashes the frog against the wall", "The princess' true love restores the prince", "The prince stays a frog for eternity", "The princess kisses the frog"]
         ]
-        let image = ["glass-slipper.gif", "snow-white-queen.gif", "rapunzel-witch.gif", "sleeping-beauty.gif", "little-marmaid.gif"];
+        let image = ["glass-slipper.gif", "snow-white-queen.gif", "rapunzel-witch.gif", "sleeping-beauty.gif", "little-marmaid.gif", "frog-prince.gif"];
         for (let i = 0; i < allHeadings.length; i++) {
             this.questions.push(new Question(allHeadings[i], allInputs[i], image[i]));
         }
     }
     displayQuestion() {
-        let questions = this.questions[this.count];
-        questionNumber.innerText = `Question ${this.count + 1}`;
-        heading.innerText = this.questions[this.count].heading;
-        image.setAttribute("src", questions.image);
-        submitButton.classList.remove("hidden");
-        nextButton.classList.add("hidden");
-        for (let i = 0; i < 4; i++) {
-            inputs[i].setAttribute("value", questions.inputs[i]);
-            inputs[i].setAttribute("name", `question ${this.count}`)
-            paragraphs[i].innerText = questions.inputs[i];
+        if (this.count < 6) {
+            let questions = this.questions[this.count];
+            questionNumber.innerText = `Question ${this.count + 1}`;
+            heading.innerText = this.questions[this.count].heading;
+            image.setAttribute("src", questions.image);
+            submitButton.classList.remove("hidden");
+            nextButton.classList.add("hidden");
+            for (let i = 0; i < 4; i++) {
+                inputs[i].setAttribute("value", questions.inputs[i]);
+                inputs[i].setAttribute("name", `question ${this.count}`)
+                paragraphs[i].innerText = questions.inputs[i];
+            }
+        }
+        else{
+            form.style.display = "none";
+            let gameOver = document.createElement("h2");
+            quizContainer.appendChild(gameOver); 
+            gameOver.innerText = `Game Over your score is ${this.score} out of 6`;
+            let message = document.createElement("h3"); 
+            quizContainer.appendChild(message); 
+           if( this.score == 6){
+               message.innerText = "A perfect score! You really know your stuff!"; 
+           } 
+           else if(this.score <6 && this.score > 3){
+               message.innerText = "Not too bad, but you can do better!"
+           }
+           else if(this.score <= 3){
+               message.innerText = "Yikes. Might wanna brush up on your fairytales";
+           }
         }
     }
+    shuffle(array){
+        var m = array.length, t,s;
+        while(m){
+          s = Math.floor(Math.random()* m--);
+          t = array[m];
+          array[m] = array[s];
+          array[s] = t;
+        }
+        return array;
+      }
     checkAnswer() {
         this.count++;
+        submitButton.classList.add("hidden");
         let questions = this.questions[this.count];
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < form.length ; i++) {
             if (form[i].checked) {
                 console.log(form[i].value)
-                //    if(form[i].value == "Cut off their toes and heels"|| form[i].value == "She was forced to wear a pair of red hot iron shoes until she dropped dead" || form[i].value == "Rapunzel's clothes no longer fit because she was pregnant"){
                 if (correct.includes(form[i].value)) {
                     body.classList.add("green");
                     nextButton.classList.remove("hidden");
-
+                    this.score++;
                 }
                 else {
                     body.classList.add("red");
@@ -85,12 +117,14 @@ class Bank {
     }
     startGame() {
         this.createQuestions();
+        this.shuffle(this.questions);
         this.displayQuestion();
         quizContainer.classList.remove("hidden");
         intro.classList.add("hidden");
-
+        startButton.classList.add("hidden");
     }
 }
+//Initializing the instance of the class Bank called newBank; 
 let newBank = new Bank();
 
 //Add Event Listener to start button for starting the quiz 
@@ -119,5 +153,6 @@ function nextQuestion(evt) {
     evt.preventDefault();
     body.classList.remove("green");
     body.classList.remove("red");
+    submitButton.classList.remove("hidden");
     newBank.displayQuestion();
 }
